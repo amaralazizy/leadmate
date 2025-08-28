1b8478fed14fd372a723af87e13133e776188079
amaralazizy <21-101152@students.eui.edu.eg>
2025-08-28 23:06:48 +0300

    Edit dashboard

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -1,24 +1,22 @@
-import * as React from "react";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   Sidebar,
   SidebarContent,
-  SidebarFooter,
+  // SidebarFooter,
   SidebarHeader,
   SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
+  // SidebarMenuItem,
   SidebarRail,
 } from "@/components/ui/sidebar";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
-import LogoutButton from "@/components/LogoutButton";
 import { AppSidebarItems } from "@/components/AppSidebarItems";
-
-export function AppSidebar({ className, ...props }: React.ComponentProps<typeof Sidebar>) {
-
+import { AppSidebarFooter } from "@/components/AppSidebarFooter";
+export function AppSidebar({
+  className,
+  ...props
+}: React.ComponentProps<typeof Sidebar>) {
   return (
-    <Sidebar collapsible="icon" {...props} className={cn("",className)}>
+    <Sidebar collapsible="icon" {...props} className={cn("", className)}>
       <SidebarHeader>
         <Image src="/logo.png" alt="LeadMate" width={120} height={120} />
       </SidebarHeader>
@@ -27,29 +25,7 @@ export function AppSidebar({ className, ...props }: React.ComponentProps<typeof 
           <AppSidebarItems />
         </SidebarMenu>
       </SidebarContent>
-      <SidebarFooter>
-        <SidebarMenu>
-          <SidebarMenuItem>
-            {/* <SidebarMenuButton
-              className="group-data-[state=collapsed]:hover:outline-0 group-data-[state=collapsed]:hover:bg-transparent overflow-visible "
-              size="lg"
-            > */}
-              <Avatar className="h-8 w-8">
-                <AvatarImage
-                  src="https://github.com/shadcn.png?size=40"
-                  alt="CN"
-                />
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
-              <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-heading"></span>
-                <span className="truncate text-xs"></span>
-              </div>
-              <LogoutButton />
-            {/* </SidebarMenuButton> */}
-          </SidebarMenuItem>
-        </SidebarMenu>
-      </SidebarFooter>
+      <AppSidebarFooter />
       <SidebarRail />
     </Sidebar>
   );
